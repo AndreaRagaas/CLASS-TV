b2404ab034c803abb0f63d968280ffe5e8c9d3a4
AndreaRagaas <andreadragaas@iskolarngbayan.pup.edu.ph>
2023-05-18 04:25:59 +0800

    Initialized two TV objects (tv1 and tv2)

--- TestTV.py
+++ TestTV.py
@@ -3,3 +3,9 @@ from TV import TV
 
 #Create a new class named TestTV
 class TestTV:
+    #Initialized two TV objects
+    def __init__(self):
+        #the first TV
+        self.tv1 = TV()
+        #the second TV
+        self.tv2 = TV()
